9779dbd827c369770811bb4bee64d38280d4f652
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-11-08 21:08:59 +0100

    created CategoryController

---
+++ src/main/java/com/example/springrestapi/category/CategoryController.java
@@ -0,0 +1,31 @@
+package com.example.springrestapi.category;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    CategoryService categoryService;
+
+    public CategoryController(CategoryService CategoryService) {
+        this.categoryService = CategoryService;
+    }
+
+    @GetMapping("/categories")
+    public List<CategoryDTO> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping("/categories")
+    public ResponseEntity<Void> createPerson(@RequestBody CategoryDTO personDto) {
+        int id = categoryService.addCategory(personDto);
+        return ResponseEntity.created(URI.create("/persons/" + id)).build();
+    }
+}
