78eac4e0744b70f0120cd779bd26ae13fd91db88
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-11-13 09:15:33 +0100

    typo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -18,4 +18,4 @@ INSERT INTO location (name, category_id, user_id, is_private, created, last_modi
     VALUES ('Bree Town Sausages', 1, 'thestrider1337', 0, NOW(), NOW(), 'Very nice pig sausages right outside the Prancing Pony Inn.', 90, 50, 0),
            ('Weather Top Ruins', 2, 'nazgul1', 0, NOW(), NOW(), 'Great spot to fry some bacon and tomatoes!', 50, 50, 0),
            ('Rivendell Gas and Bread', 3, 'elr0nd', 1, NOW(), NOW(), 'Stop for supplies and rest.', -25, 36, 0),
-           ('Bride of Kazad-Dun', 4, 'somegoblin523', 0, NOW(), NOW(), 'Apparantly the bridge has been cut in half, think its going to be out of order for a while', 1, 57, 0);
+           ('Bridge of Kazad-Dun', 4, 'somegoblin523', 0, NOW(), NOW(), 'Apparantly the bridge has been cut in half, think its going to be out of order for a while', 1, 57, 0);
